3fe5d0ce024d40dba2c4c9b92b880f5972413aec
emckinney <elderied@gmail.com>
2020-01-08 08:16:43 -0600

    first commit
    starting the project

---
+++ DogGram/DogGram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogGram
+//
+//  Created by Elderied McKinney on 1/8/20.
+//  Copyright © 2020 Elderied McKinney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
